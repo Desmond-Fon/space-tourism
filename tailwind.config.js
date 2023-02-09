a1f1fd76e66129b1e1ff6cb2dcd05d2b9f9e6fc0
Desmond-Fon <desmondfon@yahoo.com>
2023-02-09 23:44:37 +0100

    added my custom styles and set up tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'darkBlue': '#0B0D17',
+        'skyBlue': '#D0D6F9',
+        'cream': '#FFFFFF'
+      },
+      fontFamily: {
+        'bellefair': ['Bellefair', 'serif'],
+        'barlow': ['Barlow Condensed', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
